0e35dfff43c26c8c83c93e9793404765da6f9261
name <you@example.com>
2025-05-18 14:23:08 +0200

    sdvxcv

--- beca.js
+++ beca.js
@@ -1,11 +1,11 @@
-// BECA ESTUDIANTE - Versión interactiva con prompt y alert
+// Student Scholarship
 
 let age = parseInt(prompt("Enter your age:"));
 let city = prompt("In which city do you reside??").toLowerCase();
 let haveLargeFamily = prompt("Do you have a large family? (yes/no)").toLowerCase() === "yes";
 let isProgrammingGenius = prompt("Are you a programming genius? (yes/no)").toLowerCase() === "yes";
 
-let meetCondition1 = age >= 18 && city === "barcelona";
+let meetCondition1 = age >= 18 && city === "Barcelona";
 let meetCondition2 = age > 30 && haveLargeFamily;
 let meetCondition3 = age >= 12 && age <= 18 && isProgrammingGenius;
 
