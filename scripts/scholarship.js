2263b89d3759c92b54518db7720b2da11b3be1ee
name <you@example.com>
2025-05-18 15:04:24 +0200

    <

--- scripts/scholarship.js
+++ scripts/scholarship.js
@@ -5,9 +5,9 @@ let city = prompt("In which city do you reside??").toLowerCase();
 let haveLargeFamily = prompt("Do you have a large family? (yes/no)").toLowerCase() === "yes";
 let isProgrammingGenius = prompt("Are you a programming genius? (yes/no)").toLowerCase() === "yes";
 
-let meetCondition1 = age > = 18 && city === "Barcelona";
+let meetCondition1 = age > 18 & city === "Barcelona";
 let meetCondition2 = age > 30 && haveLargeFamily;
-let meetCondition3 = age > = 12 && age <= 18 && isProgrammingGenius;
+let meetCondition3 = age > 12 && age < 18 && isProgrammingGenius;
 
 if (meetCondition1 || meetCondition2 || meetCondition3) {
   alert("Congratulations! You meet the requirements for the scholarship");
